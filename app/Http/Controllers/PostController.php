cf17da9a485e48db2776425f59e11a8ec704ff63
Ariful <arifsofg@gmail.com>
2019-12-16 01:47:00 +0600

    Changed files and fix post save error

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -20,60 +20,51 @@ class PostController extends Controller
     protected $request;
     protected $post;
 
-    public function __construct(Request $request, Post $post) {
+    public function __construct(Request $request, Post $post)
+    {
 
-    $this->request = $request;
-    $this->post = $post;
-    $this->middleware('auth');
-    // $this->middleware('permission:lista-categoria');
-    // $this->middleware('permission:cria-categoria', ['only' => ['create','store']]);
-    // $this->middleware('permission:atualiza-categoria', ['only' => ['edit','update']]);
-    // $this->middleware('permission:deleta-categoria', ['only' => ['destroy']]);
-    
+        $this->request = $request;
+        $this->post = $post;
+        $this->middleware('auth');
+        // $this->middleware('permission:lista-categoria');
+        // $this->middleware('permission:cria-categoria', ['only' => ['create','store']]);
+        // $this->middleware('permission:atualiza-categoria', ['only' => ['edit','update']]);
+        // $this->middleware('permission:deleta-categoria', ['only' => ['destroy']]);
+
     }
 
-    public function index() {
+    public function index()
+    {
 
-        $posts = Post::with('autor')->with('categorias')->orderBy('id','desc')->paginate(6);
+        $posts = Post::with('autor')->with('categorias')->orderBy('id', 'desc')->paginate(6);
 
         // dd($posts);
         return view('posts.lista', compact('posts'));
-
-    
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create() {
-        
+
+    public function create()
+    {
+
         $categorias = Categoria::all();
 
         return view('posts.novo', compact('categorias'));
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request) {
 
-        // dd($request);
+    public function store(Request $request)
+    {
 
-        // validate
+        //validate
         $validator = $this->validate($request, [
             'titulo'     => 'required|unique:posts',
             'texto'      => 'required',
-            'categorias' => 'required',
+            'categorias' => 'required|array|min:1',
             'image'      => 'required'
 
         ]);
 
-        try{
+        try {
             # caminho das pastas de arquivos
             $pasta_post = 'public' . DIRECTORY_SEPARATOR . 'images' . DIRECTORY_SEPARATOR . 'posts';
 
@@ -81,80 +72,45 @@ class PostController extends Controller
 
                 $arquivo_post = $request->file('image');
                 $extensao  = $arquivo_post->getClientOriginalExtension();
-                $nome_arquivo = 'post_' . '.' . $extensao;
+                $nome_arquivo = 'post_' . rand(11111111, 99999999) . '.' . $extensao;
                 $upload = $arquivo_post->storeAs($pasta_post, $nome_arquivo);
-            
             }
 
             $post = Post::create([
                 'titulo' => $request->get('titulo'),
-                'slug'   => Str::slug($post->titulo, '-'),
-                'texto'  => $request->get('texto')
-               
+                'slug'   => Str::slug($request->get('titulo'), '-'),
+                'texto'  => $request->get('texto'),
+                'image' =>  $nome_arquivo,
             ]);
 
             $post->categorias()->sync($request->get('categorias'));
-
-            dd($post);
-                
             # status de retorno
-            Session::flash('success',' O post foi salvo com sucesso!');
-            return redirect()->route('posts.show', $post->id);  
-
-        }catch (\Exception $exception){
-
-            eg: Log::debug($exception->getMessage());
+            Session::flash('success', ' O post foi salvo com sucesso!');
+            return redirect()->route('posts.show', $post->id);
+        } catch (\Exception $exception) {
 
             # status de retorno
-            Session::flash('error',' O post não pôde ser cadastrado!'); 
+            Session::flash('error', ' O post não pôde ser cadastrado!');
 
             return redirect()->back()->withInput();
         }
 
         return redirect()->route('posts.index');
-
-
     } // end store
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id) {
-        
 
-    }
+    public function show($id)
+    { }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id) {
-        
-    }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id) {
-        
-    }
+    public function edit($id)
+    { }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id) {
-        
-    }
+
+    public function update(Request $request, $id)
+    { }
+
+
+    public function destroy($id)
+    { }
 }
